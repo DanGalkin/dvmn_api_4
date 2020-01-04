f135b86e4672d523179714dc1ce7512b4aab0277
DanGalkin <dangalkin@gmail.com>
2020-01-04 20:22:00 +0300

    download hubble photos
    add function to download photos.
    also disabled SSL verification for downloading

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ import requests
 import os
 
 def download_image(url, file_name):
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
     response.raise_for_status()
     with open(file_name, 'wb') as file:
         file.write(response.content)
+
+def download_photos(links, photos_name, folder_name):
+	if not os.path.exists(folder_name):
+		os.makedirs(folder_name)
+	for link_number, link in enumerate(links):
+		file_name = "{}/{}{}.{}".format(folder_name, photos_name, link_number, get_file_extension(link))
+		download_image(link, file_name)
+
+def get_file_extension(link):
+	return link.split('.')[-1];
 
 def get_latest_spacex_photo_links():
     api_method = "https://api.spacexdata.com/v3/launches"
@@ -19,15 +29,8 @@ def get_latest_spacex_photo_links():
                 break;
     return image_links
 
-def download_photos(links, photos_name, folder_name, format_name):
-	if not os.path.exists(folder_name):
-		os.makedirs(folder_name)
-	for link_number, link in enumerate(links):
-		file_name = "{}/{}{}.{}".format(folder_name, photos_name, link_number, format_name)
-		download_image(link, file_name)
-
 def fetch_spacex_last_launch():
-	download_photos(get_latest_spacex_photo_links(), "spacex", "images", "jpg")
+	download_photos(get_latest_spacex_photo_links(), "spacex", "images")
 
 def get_hubble_photo_links(id):
 	api_method = "http://hubblesite.org/api/v3/image/"
@@ -36,16 +39,14 @@ def get_hubble_photo_links(id):
 	image_api_response = requests.get(api_request)
 	if image_api_response.ok:
 		for file in image_api_response.json()['image_files']:
-			image_links.append(file['file_url'])
+			image_links.append("http:"+file['file_url'])
 	return image_links
 
-def get_file_extension(link):
-	return link.split('.')[-1];
-
+def fetch_hubble_photo(id):
+	download_photos(get_hubble_photo_links(id), "hubble_" + str(id) + "_", "images")
 
 def main():
-    for link in get_hubble_photo_links(1):
-    	print(link + "      " + get_file_extension(link));
+    fetch_hubble_photo(3000)
 
 if __name__ == '__main__':
     main()
