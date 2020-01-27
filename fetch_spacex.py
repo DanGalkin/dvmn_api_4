f4c6575b656d035930937af01f630be30c4bb582
DanGalkin <dangalkin@gmail.com>
2020-01-27 18:48:15 +0300

    Use os.path.splitext()
    Instead of additional function

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -21,13 +21,11 @@ def get_latest_spacex_photo_links():
 
 def download_photos_from_links(links):
     for link_number, link in enumerate(links):
-        image_file_extension = get_file_extension_from_link(link)
+        image_file_extension = os.path.splitext(link)[1]
+        print(image_file_extension)
         image_name = "{}_{}.{}".format(NAME_PREFIX, link_number, image_file_extension)
         save_path = os.path.join(IMAGES_FOLDER, image_name)
         download_image(link, save_path)
-
-def get_file_extension_from_link(link):
-    return link.split('.')[-1];
 
 def download_image(url, save_path):
     response = requests.get(url, verify=False)
